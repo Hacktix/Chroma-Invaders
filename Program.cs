597fe5c9f27fef7ff3e5b2cc0ae7b8eda206ec19
Hacktix <zecmad16@htlkaindorf.at>
2020-05-16 21:30:30 +0200

    Setup for further development

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Chroma_Invaders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

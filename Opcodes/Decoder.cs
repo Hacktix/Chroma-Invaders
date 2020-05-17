5d09d8be8029e6426190b990cadfabe4fa93a5ea
Hacktix <zecmad16@htlkaindorf.at>
2020-05-17 20:29:09 +0200

    Implemented CMC Operation

--- Opcodes/Decoder.cs
+++ Opcodes/Decoder.cs
@@ -7,6 +7,7 @@
             if ((code & 0b11000111) == 0b00000100) return new IncrementOperation(parent, code);
             if ((code & 0b11000111) == 0b00000101) return new DecrementOperation(parent, code);
             if (code == 0x37)                      return new SetCarryOperation(parent);
+            if (code == 0x3F)                      return new ComplementCarryOperation(parent);
             else return new NoOperation(parent, code);
         }
     }
---
+++ Opcodes/Operations/ComplementCarryOperation.cs
@@ -0,0 +1,12 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class ComplementCarryOperation : Opcode
+    {
+        public ComplementCarryOperation(Machine parent) : base(parent) { }
+
+        public override void Execute()
+        {
+            parent.Registers[Register.F] ^= 1;
+        }
+    }
+}

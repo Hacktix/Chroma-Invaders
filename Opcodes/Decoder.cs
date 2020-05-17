a0bf0b3dcb676fd06d1ea3719877db3a876ed02b
Hacktix <zecmad16@htlkaindorf.at>
2020-05-17 23:26:23 +0200

    Implemented DAA Operation

--- Opcodes/Decoder.cs
+++ Opcodes/Decoder.cs
@@ -4,10 +4,11 @@
     {
         public static Opcode DecodeOpcode(Machine parent, byte code)
         {
-            if ((code & 0b11000111) == 0b00000100) return new IncrementOperation(parent, code);
-            if ((code & 0b11000111) == 0b00000101) return new DecrementOperation(parent, code);
-            if (code == 0x37)                      return new SetCarryOperation(parent);
-            if (code == 0x3F)                      return new ComplementCarryOperation(parent);
+            if ((code & 0b11000111) == 0b00000100)      return new IncrementOperation(parent, code);
+            else if ((code & 0b11000111) == 0b00000101) return new DecrementOperation(parent, code);
+            else if (code == 0x37)                      return new SetCarryOperation(parent);
+            else if (code == 0x3F)                      return new ComplementCarryOperation(parent);
+            else if (code == 0x27)                      return new DecimalAdjustOperation(parent);
             else return new NoOperation(parent, code);
         }
     }
---
+++ Opcodes/Operations/DecimalAdjustOperation.cs
@@ -0,0 +1,15 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class DecimalAdjustOperation : Opcode
+    {
+        public DecimalAdjustOperation(Machine parent) : base(parent) { }
+
+        public override void Execute()
+        {
+            if (((parent.Registers[Register.A]) & 0xF) > 9 || (parent.Registers[Register.F] & (byte)Flag.AuxiliaryCarry) != 0)
+                parent.Registers[Register.A] += 6;
+            if ((((parent.Registers[Register.A]) & 0xF0) >> 4) > 9 || (parent.Registers[Register.F] & (byte)Flag.Carry) != 0)
+                parent.Registers[Register.A] += 6 << 4;
+        }
+    }
+}

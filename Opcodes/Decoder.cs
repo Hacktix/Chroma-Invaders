d3ca8f2e64f7261eef23ba6625adc2692c5d6761
Hacktix <zecmad16@htlkaindorf.at>
2020-05-17 20:24:54 +0200

    Implemented STC Operation

--- Opcodes/Decoder.cs
+++ Opcodes/Decoder.cs
@@ -6,6 +6,7 @@
         {
             if ((code & 0b11000111) == 0b00000100) return new IncrementOperation(parent, code);
             if ((code & 0b11000111) == 0b00000101) return new DecrementOperation(parent, code);
+            if (code == 0x37)                      return new SetCarryOperation(parent);
             else return new NoOperation(parent, code);
         }
     }
---
+++ Opcodes/Operations/SetCarryOperation.cs
@@ -0,0 +1,12 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class SetCarryOperation : Opcode
+    {
+        public SetCarryOperation(Machine parent) : base(parent) { }
+
+        public override void Execute()
+        {
+            parent.Registers[Register.F] |= 1;
+        }
+    }
+}

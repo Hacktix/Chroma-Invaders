a6835b4c14a305ee2f56aa14dcc69c3e9e0a975e
Hacktix <zecmad16@htlkaindorf.at>
2020-05-17 23:29:42 +0200

    Implemented Flag modification for DAA operation

--- Opcodes/Operations/DecimalAdjustOperation.cs
+++ Opcodes/Operations/DecimalAdjustOperation.cs
@@ -7,9 +7,15 @@
         public override void Execute()
         {
             if (((parent.Registers[Register.A]) & 0xF) > 9 || (parent.Registers[Register.F] & (byte)Flag.AuxiliaryCarry) != 0)
+            {
+                parent.SetFlag(Flag.AuxiliaryCarry, parent.Registers[Register.A] + 6 > 0xF);
                 parent.Registers[Register.A] += 6;
+            }
             if ((((parent.Registers[Register.A]) & 0xF0) >> 4) > 9 || (parent.Registers[Register.F] & (byte)Flag.Carry) != 0)
+            {
+                parent.SetFlag(Flag.Carry, parent.Registers[Register.A] + (6 << 4) > 0xF);
                 parent.Registers[Register.A] += 6 << 4;
+            }
         }
     }
 }

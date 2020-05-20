67d02c6214643ba982d8b286c93c97efe5449ff1
Hacktix <zecmad16@htlkaindorf.at>
2020-05-20 20:16:19 +0200

    Implemented CPI operation

--- Opcodes/Decoder.cs
+++ Opcodes/Decoder.cs
@@ -26,6 +26,7 @@
             else if ((code & 0b11111111) == 0b11100110) return new ImmediateAndOperation(parent);
             else if ((code & 0b11111111) == 0b11101110) return new ImmediateXorOperation(parent);
             else if ((code & 0b11111111) == 0b11110110) return new ImmediateOrOperation(parent);
+            else if ((code & 0b11111111) == 0b11111110) return new ImmediateCompareOperation(parent);
             else                                        return new NoOperation(parent, code);
         }
     }
---
+++ Opcodes/Operations/ImmediateCompareOperation.cs
@@ -0,0 +1,23 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class ImmediateCompareOperation : Opcode
+    {
+        public ImmediateCompareOperation(Machine parent) : base(parent) {
+            Cycles = 7;
+            Length = 2;
+        }
+
+        public override void Execute()
+        {
+            byte iv = parent.Registers[Register.A];
+            byte subv = parent.Memory[parent.PC + 1];
+
+            // Set Flags
+            parent.SetFlag(Flag.Carry, iv - subv < 0);
+            parent.SetFlag(Flag.AuxiliaryCarry, ((iv & 0xF) - (subv & 0xF)) < 0);
+            parent.SetFlag(Flag.Parity, ((byte)(iv - subv)) % 2 == 0);
+            parent.SetFlag(Flag.Zero, ((byte)(iv - subv)) == 0);
+            parent.SetFlag(Flag.Sign, (((byte)(iv - subv)) & 128) > 0);
+        }
+    }
+}

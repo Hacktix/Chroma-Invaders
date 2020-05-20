ddccc6a4da57e53662138e22505b32886c6a8e2c
Hacktix <zecmad16@htlkaindorf.at>
2020-05-20 23:55:34 +0200

    Implemented XTHL operation

---
+++ Opcodes/Operations/ExchangeStackOperation.cs
@@ -0,0 +1,17 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class ExchangeStackOperation : Opcode // XTHL
+    {
+        public ExchangeStackOperation(Machine parent) : base(parent) {
+            Cycles = 18;
+        }
+
+        public override void Execute()
+        {
+            ushort sv = (ushort)(parent.Memory[parent.SP] + (parent.Memory[parent.SP + 1] << 8));
+            parent.Memory[parent.SP] = parent.Registers[Register.L];
+            parent.Memory[parent.SP + 1] = parent.Registers[Register.H];
+            parent.WriteRegister16(OperationTarget16.H, sv);
+        }
+    }
+}

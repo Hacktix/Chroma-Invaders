9598ed892a0a086e4cbe08c0d9329d3fbe510dcf
Hacktix <zecmad16@htlkaindorf.at>
2020-05-20 23:57:08 +0200

    Implemented SPHL operation

---
+++ Opcodes/Operations/LoadSPOperation.cs
@@ -0,0 +1,14 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class LoadSPOperation : Opcode // SPHL
+    {
+        public LoadSPOperation(Machine parent) : base(parent) {
+            Cycles = 5;
+        }
+
+        public override void Execute()
+        {
+            parent.SP = parent.ReadRegister16(OperationTarget16.H);
+        }
+    }
+}

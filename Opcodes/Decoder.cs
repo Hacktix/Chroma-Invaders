bffa2831e2ba553c230a1aa1a44df6dc4c676c8e
Hacktix <zecmad16@htlkaindorf.at>
2020-05-21 16:49:35 +0200

    Implemented HLT operation

--- Opcodes/Decoder.cs
+++ Opcodes/Decoder.cs
@@ -10,7 +10,7 @@
             else if (code == 0x37)                      return new SetCarryOperation(parent);
             else if (code == 0x3F)                      return new ComplementCarryOperation(parent);
             else if (code == 0x27)                      return new DecimalAdjustOperation(parent);
-            // TODO: Add HALT Operation
+            else if ((code & 0b11111111) == 0b01110110) return new HaltOperation(parent);
             else if ((code & 0b11000000) == 0b01000000) return new MoveOperation(parent, code);
             else if ((code & 0b11100111) == 0b00000010) return new AccMoveOperation(parent, code);
             else if ((code & 0b11110000) == 0b10000000) return new AddOperation(parent, code);
---
+++ Opcodes/Operations/HaltOperation.cs
@@ -0,0 +1,13 @@
+﻿namespace Chroma_Invaders.Opcodes
+{
+    public class HaltOperation : Opcode // HLT
+    {
+        public HaltOperation(Machine parent) : base(parent) {
+            Cycles = 7;
+        }
+
+        public override void Execute() {
+            parent.Halted = true;
+        }
+    }
+}

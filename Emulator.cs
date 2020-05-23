bcbb382e8ff42475cd6cd03afeebf0794f3c8b86
Hacktix <zecmad16@htlkaindorf.at>
2020-05-23 17:39:33 +0200

    Implemented toggleable color mode, closes #4

--- Emulator.cs
+++ Emulator.cs
@@ -16,6 +16,8 @@ namespace Chroma_Invaders
 
         private Machine Machine;
 
+        private bool UseColor = true;
+
         public Emulator(byte[][] roms)
         {
             Machine = new Machine(roms);
@@ -50,6 +52,9 @@ namespace Chroma_Invaders
                     break;
                 case Chroma.Input.KeyCode.Right:
                     Machine.InputPort1 |= 0b1000000;
+                    break;
+                case Chroma.Input.KeyCode.C:
+                    UseColor = !UseColor;
                     break;
             }
         }
@@ -89,9 +94,12 @@ namespace Chroma_Invaders
                         if ((Machine.Memory[0x2400 + col * 0x20 + row] & bitmap) != 0)
                         {
                             Color pxColor = Color.White;
-                            if (y / SCALE_FACTOR > 32 && y / SCALE_FACTOR < 49) pxColor = Color.Red;
-                            else if (y / SCALE_FACTOR >= 192 && y / SCALE_FACTOR <= 239) pxColor = Color.LimeGreen;
-                            else if(y/SCALE_FACTOR > 239 && (x/SCALE_FACTOR >= 26 && x/SCALE_FACTOR <= 54)) pxColor = Color.LimeGreen;
+                            if(UseColor)
+                            {
+                                if (y / SCALE_FACTOR > 32 && y / SCALE_FACTOR < 49) pxColor = Color.Red;
+                                else if (y / SCALE_FACTOR >= 192 && y / SCALE_FACTOR <= 239) pxColor = Color.LimeGreen;
+                                else if (y / SCALE_FACTOR > 239 && (x / SCALE_FACTOR >= 26 && x / SCALE_FACTOR <= 54)) pxColor = Color.LimeGreen;
+                            }
                             context.Rectangle(ShapeMode.Fill, new Vector2(x, y), SCALE_FACTOR, SCALE_FACTOR, pxColor);
                         }
                     }
